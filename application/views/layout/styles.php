198162a5ba02e415a17fe2f2f7ef67145085bf81
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-02-05 20:03:39 -0500

    some changes

---
+++ application/views/layout/styles.php
@@ -0,0 +1,2 @@
+<!-- Bootstrap -->
+<link href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/lux/bootstrap.min.css" rel="stylesheet" integrity="sha384-oOs/gFavzADqv3i5nCM+9CzXe3e5vXLXZ5LZ7PplpsWpTCufB7kqkTlC9FtZ5nJo" crossorigin="anonymous">
