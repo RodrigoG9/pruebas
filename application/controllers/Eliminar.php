edc335c5f96c0591a41787be3781286cbdc046c8
rromom <rromom@est.ups.edu.ec>
2020-02-16 20:24:54 -0500

    interfaz de eliminar salas

--- application/controllers/Eliminar.php
+++ application/controllers/Eliminar.php
@@ -7,16 +7,20 @@ class Eliminar extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        $this->load->model(array('sala_model'));
     }
     
     public function index()
 	{
+        $salas = $this->sala_model->getSalas();
 		$vars = array(
 			'scripts' => $this->load->view('layout/scripts', '', true),
 			'styles' => $this->load->view('layout/styles', '', true),
-			'nav' => $this->load->view('layout/nav-admin', '', true)
+            'nav' => $this->load->view('layout/nav-admin', '', true),
+            'data' => $salas
 		);
-		$this->load->view('layout/home/eliminar_page', $vars);
+        $this->load->view('layout/home/eliminar_page', $vars);
+        
 	}
 
 
--- application/views/layout/home/eliminar_page.php
+++ application/views/layout/home/eliminar_page.php
@@ -10,6 +10,39 @@
 </head>
 <body class="bg-white">
     <?= $nav ?>	
+    <div class="container p-2 shadow p-3 mb-5 bg-white rounded">
+<h1 class="text-center"> Salas <h1>
+<div class="row">
+	<div class="col">
+    <table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">NOMBRE SALA</th>
+      <th scope="col">IMAGEN</th>
+      <th scope="col">ELIMINAR?</th>
+    </tr>
+    <?php
+    foreach ($data as $sala) {
+        echo "<tr>";
+        echo "<th scope=col >".
+        $sala->titulo.
+        "</th>";
+        echo "<th scope=col>";
+        echo '<img src="data:image/jpeg;base64,' . base64_encode($sala->foto_1) . '" class="card-img-topalt="..." height="70px width="70px">';
+        echo "</th>";
+        echo '<th scope=col><button type="button" class="btn btn-warning">ELIMINAR</button></th>"';
+        echo "<tr>";
+    }
+    ?>
+  </thead>
+  
+</table> 
+    </div>
+</div>
+
+
+
+
     <?= $scripts?>
 </body>
 </html>
